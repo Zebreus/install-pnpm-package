7cac7206562a810b06befddb4f4a2621f92342c3
Zebreus <lennarteichhorn@googlemail.com>
2022-07-24 19:25:58 +0200

    Install jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  coverageThreshold: {
+    global: {
+      statements: 95,
+    },
+  },
+}
+
